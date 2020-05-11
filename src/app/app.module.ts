0260448b55709b5aed1f94a87384498f043901ca
Ricardo Hofstetter Dias <ricardo.dias@lambda3.com.br>
2020-05-11 18:58:47 -0300

    removido import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppComponent } from './app.component';
 import { PhotosModule } from './photos/photos.module';
 import { AppRoutingModule } from './photos/app.routing.module';
 import { ErrorsModule } from './errors/errors.module';
-import { PhotosComponent } from './photos/photo-list/photos/photos.component';
 
 @NgModule({
   declarations: [
