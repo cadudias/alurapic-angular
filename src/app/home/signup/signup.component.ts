6a63a66109c1db6af02215bcfb62b26bcb016f89
Ricardo Hofstetter Dias <caduhd@Ricardos-MacBook-Air.local>
2020-05-26 09:41:53 -0300

    ádd service signup

--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { lowerCaseValidator } from 'src/app/shared/validators/lower-case.validator';
+import { UserNotTakenValidatorService } from './user-not-taken-validator.service';
 
 @Component({
     templateUrl: './signup.component.html'
@@ -9,7 +10,7 @@ export class SignUpComponent implements OnInit{
 
     signupForm: FormGroup
 
-    constructor(private formBuilder: FormBuilder){}
+    constructor(private formBuilder: FormBuilder, private userNotTakenValidatorService: UserNotTakenValidatorService){}
     
     ngOnInit(): void {
        this.signupForm = this.formBuilder.group({
@@ -34,7 +35,8 @@ export class SignUpComponent implements OnInit{
                     lowerCaseValidator,
                     Validators.minLength(2),
                     Validators.maxLength(30)
-                ]
+                ],
+                this.userNotTakenValidatorService.checkUserNameTaken()
             ],
             password: ['',
                 [
